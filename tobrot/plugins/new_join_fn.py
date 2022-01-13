76c261646e68b36b4c580ad58aa79f490ea4dbae
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2022-01-13 17:05:35 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -15,7 +15,7 @@ async def new_join_f(client, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('Channel', url='https://t.me/MaxxBots')
+                        InlineKeyboardButton('Support', url='https://t.me/iet_support')
                     ]
                 ]
                )
